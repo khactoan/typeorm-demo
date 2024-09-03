84139785576116ada976c92d9868298deb65505f
toandang <dang@authentia.ca>
2024-09-03 15:39:20 +0700

    refactor roles based guard

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,9 +3,10 @@ import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/database/entities/product.entity';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), NestjsFormDataModule],
   providers: [ProductsService],
   controllers: [ProductsController],
 })
