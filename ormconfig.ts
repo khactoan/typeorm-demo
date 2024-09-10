5977e7c5177898b56cdc57b230141f4e72ba149b
toandang <dang@authentia.ca>
2024-09-10 17:31:50 +0700

    config postgres

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,12 +4,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const DatabaseConfig: DataSourceOptions = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // disable rejection of unauthorized certificates, optional
+  },
 };
 
 const AppDataSource = new DataSource({
